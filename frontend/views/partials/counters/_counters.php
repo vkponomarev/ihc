5b36ecc38608f2c6f4b0defdc1c3996814e2011f
vkponomarev <vkponomarev@gmail.com>
2020-03-31 17:00:36 +0500

    Добавление счетчиков

--- frontend/views/partials/counters/_counters.php
+++ frontend/views/partials/counters/_counters.php
@@ -1,40 +1,41 @@
 <?php
 
 /* @var $this \yii\web\View */
+
+use yii\widgets\Pjax;
+
 /* @var $content string */
 
 
 
-//AppAsset::register($this);
-//echo $this->params['title'];
-/*
- * <!-- Yandex.Metrika counter -->
+
+?>
+
+<?php Pjax::begin(); ?>
+<!-- Yandex.Metrika counter -->
 <script type="text/javascript" >
     (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
         m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
     (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-    ym(56282998, "init", {
+    ym(61507534, "init", {
         clickmap:true,
         trackLinks:true,
         accurateTrackBounce:true,
         webvisor:true
     });
 </script>
-<noscript><div><img src="https://mc.yandex.ru/watch/56282998" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<noscript><div><img src="https://mc.yandex.ru/watch/61507534" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-162297789-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
-<!--LiveInternet counter--><script type="text/javascript">
-    new Image().src = "//counter.yadro.ru/hit?r"+
-        escape(document.referrer)+((typeof(screen)=="undefined")?"":
-            ";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
-            screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
-        ";h"+escape(document.title.substring(0,150))+
-        ";"+Math.random();</script><!--/LiveInternet-->
- *
- * */
+        gtag('config', 'UA-162297789-1');
+    </script>
 
-?>
-
-
+<?php Pjax::end(); ?>
